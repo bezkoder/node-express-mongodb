7f48b1992448c8a5db31e512f6efdf973eefbbad
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2020-02-02 10:11:36 +0700

    update Mongoose model to return id instead of _id

--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -1,15 +1,19 @@
 module.exports = mongoose => {
-  const Tutorial = mongoose.model(
-    "tutorial",
-    mongoose.Schema(
-      {
-        title: String,
-        description: String,
-        published: Boolean
-      },
-      { timestamps: true }
-    )
+  var schema = mongoose.Schema(
+    {
+      title: String,
+      description: String,
+      published: Boolean
+    },
+    { timestamps: true }
   );
 
+  schema.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Tutorial = mongoose.model("tutorial", schema);
   return Tutorial;
 };
